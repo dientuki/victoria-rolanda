5516228202c79dc57369304e7d7d27acf3521046
Juan Farias <dientuki@gmail.com>
2012-02-15 23:02:14 -0300

    more home

--- wp-content/themes/victoria-rolanda/sidebar-left.php
+++ wp-content/themes/victoria-rolanda/sidebar-left.php
@@ -1,3 +1,4 @@
 <aside class="sidebar" id="sidebar-left">
-
+	<section id="day-tweet" class="block"></section>
+	<section id="poll" class="block"></section>
 </aside>
--- wp-content/themes/victoria-rolanda/sidebar-right.php
+++ wp-content/themes/victoria-rolanda/sidebar-right.php
@@ -1,3 +1,6 @@
 <aside class="sidebar" id="sidebar-right">
-
+	<section id="pov" class="block"></section>
+	<div id="banner300x200" class="banner" role="banner"></div>
+	<section id="ranking" class="block"></section>
+	<section id="follow-fb" class="block"></section>
 </aside>
