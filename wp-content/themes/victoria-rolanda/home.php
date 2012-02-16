3d8561c026cc474f97a1524546bcb7a408bc0aa5
Juan Farias <dientuki@gmail.com>
2012-02-15 21:48:10 -0300

    more to go

--- wp-content/themes/victoria-rolanda/home.php
+++ wp-content/themes/victoria-rolanda/home.php
@@ -2,12 +2,12 @@
 
 <div id="container" class="wrapper clearfix">
   <div class="container clearfix">
-    <section id="home-slideshow"></section>
+    <section id="home-slideshow" class="slideshow"></section>
     <section id="river-news">
       <?php get_template_part('river-news') ?>
     </section>
-    <section id="footer-slideshow"></section>
     <?php get_sidebar('left'); ?>
+    <section id="footer-slideshow" class="slideshow"></section>
   </div> 
   <?php get_sidebar('right'); ?>
 </div>
