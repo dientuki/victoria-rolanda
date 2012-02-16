2f97a6df23f42e60ad91b5117e3676da84b6f34a
Juan Farias <dientuki@gmail.com>
2012-02-15 22:01:27 -0300

    fonts

--- wp-content/themes/victoria-rolanda/header.php
+++ wp-content/themes/victoria-rolanda/header.php
@@ -28,6 +28,7 @@
 
 	?></title>
 <link rel="profile" href="http://gmpg.org/xfn/11" />
+<link rel="stylesheet" type="text/css" media="all" href="<?php bloginfo( 'template_url' ); ?>/css/fonts.css" />
 <link rel="stylesheet" type="text/css" media="all" href="<?php bloginfo( 'template_url' ); ?>/css/normalize.css" />
 <link rel="stylesheet" type="text/css" media="all" href="<?php bloginfo( 'template_url' ); ?>/css/style.css" />
 <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
