a0aee00d12a8912e9587f707b95fb6f79096c49b
Juan Farias <dientuki@gmail.com>
2012-02-18 00:35:41 -0300

    fix

---
+++ post.php
@@ -0,0 +1,162 @@
+<!doctype html>
+<!--[if lt IE 7]> <html class="no-js oldie ie6" lang="es"> <![endif]-->
+<!--[if IE 7]>    <html class="no-js oldie ie7" lang="es"> <![endif]-->
+<!--[if IE 8]>    <html class="no-js oldie ie8" lang="es"> <![endif]-->
+<!--[if IE 9]>    <html class="no-js ie9" lang="es"> <![endif]-->
+<!--[if gt IE 9]><!--> <html class="no-js" lang="es"> <!--<![endif]-->
+<head>
+<meta charset="UTF-8" />
+
+<body>
+
+<div style="text-align: center;">
+	<div style="text-align: justify;"></div>
+	<div class="MsoNormal" style="text-align: justify;">
+		<b><span
+			style="font-family: &amp; quot; Arial &amp;quot; ,&amp; quot; sans-serif &amp;quot;; font-size: 12pt; line-height: 115%;"><br />
+		</span>
+		</b>
+	</div>
+	<div style="text-align: justify;"><div style="text-align: justify;"></div></div>
+	<div class="MsoNormal"
+		style="font-family: &amp; quot; Trebuchet MS&amp;quot; , sans-serif; text-align: justify;">
+		<span style="font-size: small;"><span style="line-height: 115%;"><b>¿Sabías
+					que a ellos les encantan las mujeres que toman lainiciativa?</b>
+				Entre tantas propuestas que hay por el&nbsp; mes de los
+				enamorados,&nbsp; tenemos una idea para darte. ¿Y si tomás
+				ladelantera y te declarás a él con la excusa de San Valentín? ¿Cómo?
+				¿Y si lohacés por facebook?</span>
+		</span>
+	</div>
+	<div class="MsoNormal"
+		style="font-family: &amp; quot; Trebuchet MS&amp;quot; , sans-serif; text-align: justify;">
+		<br />
+	</div>
+	<div style="font-family: &amp; quot;"></div>
+	<div class="MsoNormal"
+		style="font-family: &amp; quot; Trebuchet MS&amp;quot; , sans-serif; text-align: justify;">
+		<span style="font-size: small;"><span style="line-height: 115%;">Cada
+				historia que empieza tiene un principio distinto.¡Que el tuyo sea
+				diferente a todos! Mandale una serenata.</span>
+		</span>
+	</div>
+	<div class="separator" style="clear: both; text-align: center;">
+		<a
+			href="http://4.bp.blogspot.com/-C3OIniKs3z0/TzvK-Xyr2UI/AAAAAAAABsY/CHeWwc8b9lg/s1600/serenata_amor_web.jpg"
+			imageanchor="1"
+			style="clear: right; float: right; margin-bottom: 1em; margin-left: 1em;"><img
+			border="0" height="251"
+			src="http://4.bp.blogspot.com/-C3OIniKs3z0/TzvK-Xyr2UI/AAAAAAAABsY/CHeWwc8b9lg/s320/serenata_amor_web.jpg"
+			width="320" />
+		</a>
+	</div>
+	<div class="MsoNormal"
+		style="font-family: &amp; quot; Trebuchet MS&amp;quot; , sans-serif; text-align: justify;">
+		<br />
+	</div>
+	<div
+		style="font-family: &amp; quot; Trebuchet MS&amp;quot; , sans-serif; text-align: justify;"></div>
+	<div class="MsoNormal"
+		style="background: none repeat scroll 0% 0% white; font-family: &amp; quot; Trebuchet MS&amp;quot; , sans-serif; text-align: justify;">
+		<span style="font-size: small;">Heineken aprovechaesta fecha para
+			invitarte a armar tu propia serenata para tu amado en su páginade
+			facebook. ¡Sí! A través de un recurso muy lúdico&nbsp; <span
+			style="color: black;">se permite a losconsumidores de <b>Heineken</b>&nbsp;crear
+				una&nbsp;<a href="http://apps.facebook.com/heinekenserenade/%20">Serenata
+					personalizada</a> e interpretada por la orquesta del comercial The
+				Date. Es una aplicación “dondepodrá decir cosas que no se animaría a
+				expresarlas de otra manera”, aseguróMariano Bearzi, &nbsp;Country
+				Manager Argentina.
+		</span>
+		</span>
+	</div>
+	<div
+		style="font-family: &amp; quot; Trebuchet MS&amp;quot; , sans-serif; text-align: justify;"></div>
+	<div class="MsoNormal"
+		style="background: none repeat scroll 0% 0% white; font-family: &amp; quot; Trebuchet MS&amp;quot; , sans-serif; margin-bottom: 0.0001pt; text-align: justify;">
+		<br />
+	</div>
+	<div
+		style="font-family: &amp; quot; Trebuchet MS&amp;quot; , sans-serif; text-align: justify;"></div>
+	<div class="MsoNormal"
+		style="background: none repeat scroll 0% 0% white; font-family: &amp; quot; Trebuchet MS&amp;quot; , sans-serif; margin-bottom: 0.0001pt; text-align: justify;">
+		<span style="font-size: small;"><span style="color: black;">¿Y cómo
+				sigue después? En la página te realizan una serie de
+				preguntasbásicas. ¿Por qué te gustaría salir con esa persona? ¿Qué
+				te gustaría hacer?,etc. Una vez creada, se posteará en el muro de la
+				otra persona, quién aceptaráo no la salida y posteará su respuesta
+				en su muro.</span>
+		</span>
+	</div>
+	<div
+		style="font-family: &amp; quot; Trebuchet MS&amp;quot; , sans-serif; text-align: justify;"></div>
+	<div class="MsoNormal"
+		style="background: none repeat scroll 0% 0% white; font-family: &amp; quot; Trebuchet MS&amp;quot; , sans-serif; margin-bottom: 0.0001pt; text-align: justify;">
+		<br />
+	</div>
+	<div
+		style="font-family: &amp; quot; Trebuchet MS&amp;quot; , sans-serif; text-align: justify;"></div>
+	<div class="MsoNormal"
+		style="background: none repeat scroll 0% 0% white; font-family: &amp; quot; Trebuchet MS&amp;quot; , sans-serif; margin-bottom: 0.0001pt; text-align: justify;">
+		<span style="font-size: small;"><span style="color: black;">¡Declarate!
+				Cambiá tu actitud.</span>
+		</span>
+	</div>
+	<div class="MsoNormal"
+		style="background: none repeat scroll 0% 0% white; font-family: &amp; quot; Trebuchet MS&amp;quot; , sans-serif; margin-bottom: 0.0001pt; text-align: justify;">
+		<span style="font-size: small;"><span style="color: black;">&nbsp; </span>
+		</span>
+	</div>
+	<div class="MsoNormal"
+		style="background: none repeat scroll 0% 0% rgb(224, 102, 102); font-family: &amp; quot; Trebuchet MS&amp;quot; , sans-serif; margin-bottom: 0.0001pt; text-align: justify;">
+		<span style="font-size: small;"><span style="color: black;">Hacele tu
+				serenata personalizada:</span>
+		</span><a href="http://apps.facebook.com/heinekenserenade/">
+			http://apps.facebook.com/heinekenserenade/</a>
+	</div>
+	<span style="line-height: 18px;"></span><br />
+	<br />
+	<br />
+	<span style="font-size: x-small;">Hacete fan de Victoria Rolanda en
+		Facebook: <a href="http://www.facebook.com/RevistaVictoriaRolanda">www.facebook.com/RevistaVictoriaRolanda</a>
+	</span><br />
+	<span style="font-size: x-small;">&nbsp;</span><span
+		class="Apple-style-span" style="font-size: x-small;"><a
+		href="http://www.twitter.com/RevistaVictoriaRolanda">www.twitter.com/RevistaVictoriaRolanda</a>
+	</span>
+</div>
+
+-------------------------------------------------
+
+<?php 
+$html = '<div style="text-align: center;"><div style="text-align: justify;"></div><div class="MsoNormal" style="text-align: justify;"><b><span style="font-family: &quot;Arial&quot;,&quot;sans-serif&quot;; font-size: 12pt; line-height: 115%;"><br /></span></b></div><div style="text-align: justify;"></div><div class="MsoNormal" style="font-family: &quot;Trebuchet MS&quot;,sans-serif; text-align: justify;"><span style="font-size: small;"><span style="line-height: 115%;"><b>¿Sabías que a ellos les encantan las mujeres que toman lainiciativa?</b> Entre tantas propuestas que hay por el&nbsp; mes de los enamorados,&nbsp; tenemos una idea para darte. ¿Y si tomás ladelantera y te declarás a él con la excusa de San Valentín? ¿Cómo? ¿Y si lohacés por facebook?</span></span></div><div class="MsoNormal" style="font-family: &quot;Trebuchet MS&quot;,sans-serif; text-align: justify;"><br /></div><div style="font-family: &quot;Trebuchet MS&quot;,sans-serif;"></div><div class="MsoNormal" style="font-family: &quot;Trebuchet MS&quot;,sans-serif; text-align: justify;"><span style="font-size: small;"><span style="line-height: 115%;">Cada historia que empieza tiene un principio distinto.¡Que el tuyo sea diferente a todos! Mandale una serenata.</span></span></div><div class="separator" style="clear: both; text-align: center;"><a href="http://4.bp.blogspot.com/-C3OIniKs3z0/TzvK-Xyr2UI/AAAAAAAABsY/CHeWwc8b9lg/s1600/serenata_amor_web.jpg" imageanchor="1" style="clear: right; float: right; margin-bottom: 1em; margin-left: 1em;"><img border="0" height="251" src="http://4.bp.blogspot.com/-C3OIniKs3z0/TzvK-Xyr2UI/AAAAAAAABsY/CHeWwc8b9lg/s320/serenata_amor_web.jpg" width="320" /></a></div><div class="MsoNormal" style="font-family: &quot;Trebuchet MS&quot;,sans-serif; text-align: justify;"><br /></div><div style="font-family: &quot;Trebuchet MS&quot;,sans-serif; text-align: justify;"></div><div class="MsoNormal" style="background: none repeat scroll 0% 0% white; font-family: &quot;Trebuchet MS&quot;,sans-serif; text-align: justify;"><span style="font-size: small;">Heineken aprovechaesta fecha para invitarte a armar tu propia serenata para tu amado en su páginade facebook. ¡Sí! A través de un recurso muy lúdico&nbsp; <span style="color: black;">se permite a losconsumidores de <b>Heineken</b>&nbsp;crear una&nbsp;<a href="http://apps.facebook.com/heinekenserenade/%20">Serenata personalizada</a> e interpretada por la orquesta del comercial The Date. Es una aplicación “dondepodrá decir cosas que no se animaría a expresarlas de otra manera”, aseguróMariano Bearzi, &nbsp;Country Manager Argentina.</span></span></div><div style="font-family: &quot;Trebuchet MS&quot;,sans-serif; text-align: justify;"></div><div class="MsoNormal" style="background: none repeat scroll 0% 0% white; font-family: &quot;Trebuchet MS&quot;,sans-serif; margin-bottom: 0.0001pt; text-align: justify;"><br /></div><div style="font-family: &quot;Trebuchet MS&quot;,sans-serif; text-align: justify;"></div><div class="MsoNormal" style="background: none repeat scroll 0% 0% white; font-family: &quot;Trebuchet MS&quot;,sans-serif; margin-bottom: 0.0001pt; text-align: justify;"><span style="font-size: small;"><span style="color: black;">¿Y cómo sigue después? En la página te realizan una serie de preguntasbásicas. ¿Por qué te gustaría salir con esa persona? ¿Qué te gustaría hacer?,etc. Una vez creada, se posteará en el muro de la otra persona, quién aceptaráo no la salida y posteará su respuesta en su muro.</span></span></div><div style="font-family: &quot;Trebuchet MS&quot;,sans-serif; text-align: justify;"></div><div class="MsoNormal" style="background: none repeat scroll 0% 0% white; font-family: &quot;Trebuchet MS&quot;,sans-serif; margin-bottom: 0.0001pt; text-align: justify;"><br /></div><div style="font-family: &quot;Trebuchet MS&quot;,sans-serif; text-align: justify;"></div><div class="MsoNormal" style="background: none repeat scroll 0% 0% white; font-family: &quot;Trebuchet MS&quot;,sans-serif; margin-bottom: 0.0001pt; text-align: justify;"><span style="font-size: small;"><span style="color: black;">¡Declarate! Cambiá tu actitud.</span></span></div><div class="MsoNormal" style="background: none repeat scroll 0% 0% white; font-family: &quot;Trebuchet MS&quot;,sans-serif; margin-bottom: 0.0001pt; text-align: justify;"><span style="font-size: small;"><span style="color: black;">&nbsp; </span></span></div><div class="MsoNormal" style="background: none repeat scroll 0% 0% rgb(224, 102, 102); font-family: &quot;Trebuchet MS&quot;,sans-serif; margin-bottom: 0.0001pt; text-align: justify;"><span style="font-size: small;"><span style="color: black;">Hacele tu serenata personalizada:</span></span><a href="http://apps.facebook.com/heinekenserenade/"> http://apps.facebook.com/heinekenserenade/</a></div><span style="line-height: 18px;"></span><br /><br /><br /><span style="font-size: x-small;">Hacete fan de Victoria Rolanda en Facebook: <a href="http://www.facebook.com/RevistaVictoriaRolanda">www.facebook.com/RevistaVictoriaRolanda</a></span><br /><span style="font-size: x-small;">&nbsp;</span><span class="Apple-style-span" style="font-size: x-small;"><a href="http://www.twitter.com/RevistaVictoriaRolanda">www.twitter.com/RevistaVictoriaRolanda</a></span></div>';
+
+//$html = 'Hoy fue un día muy especial.';
+
+//$html = '<div class="texto"></div>aaaa   <div class="MsoNormal" style="background: none repeat scroll 0% 0% white; font-family: &amp; quot; Trebuchet MS&amp;quot; , sans-serif; margin-bottom: 0.0001pt; text-align: justify;"> <span style="font-size: small;"> <span style="color: black;"> ¡Declarate! Cambiá tu actitud. </span> </span> </div>';
+
+/*
+$html = preg_replace('/<b>/', '<strong>', $html);
+$html = preg_replace('/<\/b>/', '</strong>', $html);
+
+$html = preg_replace('/<br \/>/', "\n", $html);
+
+//$html = preg_replace('/<div(.*)><\/div>/', '', $html);
+
+$html = preg_replace('/<\/div>/', '', $html);
+$html = preg_replace('/<\/span>/', '', $html);
+
+//$html = preg_replace('/<span(.*)>/', '', $html);
+
+//$html = preg_replace('/<div(.*)>/', '', $html);
+*/
+
+echo strip_tags($html, '<a><b><br>');
+
+//echo $html;
+
+
+?>
+
+</body>
+</html>
--- wp-content/themes/victoria-rolanda/sidebar-right.php
+++ wp-content/themes/victoria-rolanda/sidebar-right.php
@@ -1,6 +1,19 @@
 <aside class="sidebar" id="sidebar-right">
-	<section id="pov" class="block"></section>
-	<div id="banner300x200" class="banner" role="banner"></div>
+	<section id="pov" class="block">
+		<header>Puntos de vista</header>
+		<?php 
+		$args = array();
+		$arg['parent'] = 1;
+		$arg['hierarchical'] = false;
+		$categories = get_categories( $args );
+		?> 
+		<ul>
+			<?php foreach ($categories as $category) :?>
+				<li><a href="#"><?php echo $category->cat_name; ?></a></li>
+			<?php endforeach;?>
+		</ul>
+	</section>
+	<div id="banner300x200" class="banner block" role="banner"></div>
 	<section id="ranking" class="block"></section>
 	<section id="follow-fb" class="block"></section>
 </aside>
