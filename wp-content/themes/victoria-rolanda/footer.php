7b2110f65aebb6215656510fdea318612b4701b0
Juan Farias <dientuki@gmail.com>
2012-04-03 07:00:02 -0300

    add twitter

--- wp-content/themes/victoria-rolanda/footer.php
+++ wp-content/themes/victoria-rolanda/footer.php
@@ -11,13 +11,17 @@
 	<script src="<?php bloginfo( 'template_url' ); ?>/js/libs/jquery-1.7.1.min.js"></script>
 	<script src="<?php bloginfo( 'template_url' ); ?>/js/plugins/jcarousellite-1-0-1.js"></script>
 	<script src="<?php bloginfo( 'template_url' ); ?>/js/init.js"></script>
-	<script>(function(d, s, id) {
-	  var js, fjs = d.getElementsByTagName(s)[0];
-	  if (d.getElementById(id)) return;
-	  js = d.createElement(s); js.id = id;
-	  js.src = "//connect.facebook.net/es_LA/all.js#xfbml=1&appId=181963141873249";
-	  fjs.parentNode.insertBefore(js, fjs);
-	}(document, 'script', 'facebook-jssdk'));</script>
+	<script>
+		(function(d, s, id) {
+		  var js, fjs = d.getElementsByTagName(s)[0];
+		  if (d.getElementById(id)) return;
+		  js = d.createElement(s); js.id = id;
+		  js.src = "//connect.facebook.net/es_LA/all.js#xfbml=1&appId=181963141873249";
+		  fjs.parentNode.insertBefore(js, fjs);
+		}(document, 'script', 'facebook-jssdk'));
+		
+		!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src="//platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");	
+	</script>
 		
 </body>
 </html>
--- wp-content/themes/victoria-rolanda/river-news.php
+++ wp-content/themes/victoria-rolanda/river-news.php
@@ -32,7 +32,7 @@
 		      </figure>
 		      <figcaption class="hidden"><?php the_title(); ?></figcaption>
 		    		    
-		      <?php comments_popup_link( '0 comentarios', '1 comentario', '% comentarios', 'comments'); ?>
+		      <?php comments_popup_link( '<span class="arrow"></span> 0 comentarios', '<span class="arrow"></span> 1 comentario', '<span class="arrow"></span> % comentarios', 'comments'); ?>
 		    </div>
 		    
 		    
@@ -40,7 +40,9 @@
 		      <div class="fb item">
 						<div class="fb-like" data-href="<?php the_permalink(); ?>" data-send="false" data-layout="button_count" data-width="450" data-show-faces="false" data-action="recommend" data-font="arial"></div>		      
 		      </div>
-		      <div class="tw item">twitter</div>
+		      <div class="tw item">
+						<a href="https://twitter.com/share" class="twitter-share-button" data-url="<?php the_permalink(); ?>" data-text="<?php the_title(); ?>" data-via="victoriarolanda" data-lang="es">Twittear</a>
+					</div>
 		    </div>
 		
 		    <footer>
