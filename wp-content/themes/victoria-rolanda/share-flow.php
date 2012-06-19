3a820e5173b7b392dee948cbab9fddc8f230f402
Juan Alberto Farias <dientuki@gmail.com>
2012-06-19 18:23:19 -0300

    pinterest

--- wp-content/themes/victoria-rolanda/share-flow.php
+++ wp-content/themes/victoria-rolanda/share-flow.php
@@ -5,4 +5,7 @@
       <div class="tw item">
 				<a href="https://twitter.com/share" class="twitter-share-button" data-url="<?php the_permalink(); ?>" data-text="<?php the_title(); ?>" data-via="victoriarolanda" data-lang="es">Twittear</a>
 			</div>
+			<div class="pi item">
+				<a href="http://pinterest.com/pin/create/button/?url=<?php echo urlencode(get_permalink()); ?>&media=<?php echo urlencode(get_permalink()) ?>&desc=<?php echo urlencode(get_the_title());?>" class="pin-it-button" count-layout="horizontal"><img border="0" src="http://assets.pinterest.com/images/PinExt.png" title="Pin It" /></a>
+			</div>
     </div>
