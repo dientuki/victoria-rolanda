7d604d3b4a669ee059a7f691c55aa802b0ce0fd1
Juan Farias <dientuki@gmail.com>
2012-06-26 00:40:49 -0300

    crossbrowser

--- wp-content/themes/victoria-rolanda/searchform.php
+++ wp-content/themes/victoria-rolanda/searchform.php
@@ -1,4 +1,4 @@
-	<form method="get" action="<?php echo esc_url( home_url( '/' ) ); ?>">
+	<form class="search-form" method="get" action="<?php echo esc_url( home_url( '/' ) ); ?>">
 		<label for="s" class="assistive-text">Buscar:</label>
 		<input type="text" class="field" name="s" id="s" placeholder="Buscar" value="<?php echo get_search_query(); ?>" />
 		<input type="submit" class="submit" name="submit" id="searchsubmit" value="Buscar" />
